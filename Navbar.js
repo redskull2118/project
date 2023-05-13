6bb65fa05c6aa6d444ab8cd937de025353775ec2
redskull2118 <118445883+redskull2118@users.noreply.github.com>
2023-05-13 16:32:10 +0530

    Add files via upload

---
+++ Navbar.js
@@ -0,0 +1,42 @@
+
+function Navbar(){
+    return(
+        <>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">LMS</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Something</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Something</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Admin
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="#">Register as Student</a></li>
+            <li><a class="dropdown-item" href="#">Register as Warden</a></li>
+            <li><a class="dropdown-item" href="#">Register as HOD</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+        </>
+    )
+}
+
+export default Navbar;
