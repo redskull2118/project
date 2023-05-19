29384d31cc197cdc3a90c4079083f3e46b0913e8
redskull2118 <118445883+redskull2118@users.noreply.github.com>
2023-05-19 13:45:04 +0530

    Add files via upload

--- Student.js
+++ Student.js
@@ -1,145 +1,73 @@
-import { useEffect, useState } from 'react';
-import StudentApplyLeaveNavbar from './StudentApplyLeaveNavbar'
-function Student(){
-    const [users, setUsers] = useState([]);
-  
-    async function login(){
-  
-      const response = await fetch('http://localhost:8000/abc', {
-        method:'GET',
-        headers:{
-          'Content-Type':'application/json'
-        }
-       
-      });
-      const actualdata = await response.json();
-      setUsers(actualdata.data);
-      console.log(actualdata.data);
-      // return data;
-    } 
 
-    useEffect(()=>{
-      login();
-    },[])
-
-    const color=(a)=>{
-      if(a=='pending')
-      {
-        return(
-          <>
-          <p style={{color:"yellow"}}> Pending</p>
-          </>
-        )
-      }
-      else if(a=='approved')
-      {
-        return(
-          <>
-          <p style={{color:"green"}}>Approved</p>
-          </>
-        )
-      }
-      else{
-        return(
-          <>
-          <p style={{color:"red"}}>Rejected</p>
-          </>
-        )
-
-      }
-    }
-
+// import mongoose from 'mongoose';
+const mongoose=require('mongoose');
+const bcrypt=require('bcrypt');
+const jwt= require("jsonwebtoken");
+const studentSchema = mongoose.Schema({
    
-    return(
-        <>
-      <StudentApplyLeaveNavbar/>
-      <h1>Working DAY LEAVE</h1>
-      <table class="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">Student Name</th>
-      <th scope="col">Start Date</th>
-      <th scope="col">End Date</th>
-      <th scope="col">Type of Leave</th>
-      <th scope="col"> WARDEN's Status</th>
-      <th scope="col"> HOD's Status</th>
-      
-    </tr>
-  </thead>
-  <tbody>
-  {users.map(a=>{
-          return(
-            <tr>
-      <td>{a.studentname}</td>
-      <td>{a.startdate}</td>
-      <td>{a.enddate}</td>
-      <td>{a.purpose}</td>
-      <td>{color(a.status)}</td>
-      <td>{color(a.status)}</td>
-    </tr>
-
-
-        // <li>{a.studentname}</li>
-         
-        )  
-      })}
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    MobileNumber: {
+        type: String,
+        required: true,
+    },
+    Department: {
+        type: String,
+        required: true,
+    },
+    Parentscontact: {
+        type: String,
+        required: true,
+    },
+    StudentId: {
+        type: String,
+        required: true,
+        unique:true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    tokens:[
+        {
+            token:{
+                type:String,
+                required:true
+            }
+        }
+    ]
     
-    
-  </tbody>
+}, {
+    timestamps: true
+})
 
-</table>
+studentSchema.pre('save' ,async function(next){
+    if(this.isModified('password'))
+    {
+        this.password= await bcrypt.hash(this.password,12);
+    }
+    next();
+});
 
-<h1>HOLIDAY LEAVE</h1>
-
-<table class="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">Student Name</th>
-      <th scope="col">Start Date</th>
-      <th scope="col">End Date</th>
-      <th scope="col">Type of Leave</th>
-      <th scope="col"> WARDEN's Status</th>
-      <th scope="col"> HOD's Status</th>
-      
-    </tr>
-  </thead>
-  <tbody>
-  {users.map(a=>{
-          return(
-            <tr>
-      <td>{a.studentname}</td>
-      <td>{a.startdate}</td>
-      <td>{a.enddate}</td>
-      <td>{a.purpose}</td>
-      <td>{color(a.status)}</td>
-      <td>Null</td>
-    </tr>
-
-
-        // <li>{a.studentname}</li>
-         
-        )  
-      })}
-    
-    
-  </tbody>
-</table>
-
-
-
-      {/* <ul>
-      {users.map(a=>{
-          return(
-        <li>{a.studentname}</li>
-         
-        )  
-      })}
-        </ul> */}
-          
-      
-        </>
-    ) 
+studentSchema.methods.generateAuthToken= async function(){
+    try{
+        const token= jwt.sign({_id:this._id},"MYNAMEISPARVEZKHANPRANALIVANIHALDWANIKATHGODAM");
+        this.tokens=this.tokens.concat({token:token});
+        await this.save();
+        console.log(token);
+        return token;
+    }
+    catch(err){
+        res.send(err);
+    }
 }
 
+const Student = mongoose.model('Students', studentSchema)
 
-export default Student;
+module.exports=Student;
