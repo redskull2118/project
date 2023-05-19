6d494e58e6edaf6fb0c56a4dee62db17cee24745
redskull2118 <118445883+redskull2118@users.noreply.github.com>
2023-05-19 13:05:56 +0530

    Add files via upload

--- Home.js
+++ Home.js
@@ -1,80 +1,48 @@
-import Navbar from "./Navbar";
 import {Link} from 'react-router-dom';
-import "../App.css";
+import StudentApplyLeaveNavbar from './StudentApplyLeaveNavbar'
+// import WorkingDayLeave from './WorkingDayLeave'
 
 function Home(){
     return(
-        <div style={{backgroundColor:"whitesmoke"}}>
-       
-        <center>
-  <div class="card" style={{backgroundColor:"#007a99"}}>
+        <>
+        <StudentApplyLeaveNavbar/>
+        <div style={{display:'flex', marginLeft:30+'px',marginTop:40+'px', border:1+'px solid black',marginRight:30+'px' }}>
+        <div class="card"  style={{width: 18+'rem',marginLeft:110+'px'}}>
   <div class="card-body">
-   <h1 style={{color:"white"}}>Leave Management System</h1> 
+    <h1 class="card-title">Leaves</h1>
+    <h6 class="card-subtitle mb-2 text-body-secondary">  </h6>
+    <p class="card-text"><h4>Remaining Leave: 20/20</h4></p>
+    
   </div>
 </div>
-</center>
 
-  <div style={{display:'flex', marginLeft:30+'px',marginTop:100+'px',marginRight:30+'px'}}>
-  <div class="card" style={{width: 20+'rem', marginLeft:110+'px', marginRight:30+'px'}} >
-  <img src="https://www.albertamusic.org/wp-content/uploads/icon-blu-company.png"  class="card-img-top" alt="..."/>
+<div class="card"  style={{width: 18+'rem',marginLeft:110+'px'}}>
   <div class="card-body">
-    <h3 class="card-title"><b>Student Portal</b></h3>
-    <Link to="Login" style={{backgroundColor:"#007a99"}} class="btn btn-primary"><b>Login as Student</b></Link>
-
-  </div>
-</div> 
-
-<div class="card" style={{width: 20+'rem',marginLeft:110+'px',marginRight:30+'px'}}>
-<img src="https://www.albertamusic.org/wp-content/uploads/icon-blu-company.png" class="card-img-top" alt="..."/>
- <div class="card-body">
-    <h3 class="card-title"><b>Warden Portal</b></h3>
-    <Link to="/wardenregister" style={{backgroundColor:"#007a99"}} class="btn btn-primary"><b>Login as Warden</b></Link>
+    <h5 class="card-title">Working Day Leave </h5>
+    <h6 class="card-subtitle mb-2 text-body-secondary"></h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    {/* <Link to="/studentlogin">W D L</Link> */}
+    <Link to="/studentworkingdayleave">W D L</Link>
+    {/* <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a> */}
   </div>
 </div>
 
-<div class="card" style={{width: 20+'rem',marginLeft:110+'px'}}>
-<img src="https://www.albertamusic.org/wp-content/uploads/icon-blu-company.png" class="card-img-top" alt="..."/>
+<div class="card"  style={{width: 18+'rem',marginLeft:110+'px'}}>
   <div class="card-body">
-    <h3 class="card-title"><b>HOD Portal</b></h3>
-    <Link to="/Hodregister" style={{backgroundColor:"#007a99"}} class="btn btn-primary"><b>Login as HOD</b></Link>
- 
-   
+    <h5 class="card-title">Holiday Leave</h5>
+    <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
   </div>
 </div>
 </div>
-{/* <div class="row row-cols-1 row-cols-md-3 g-4">
-  <div class="col">
-    <div class="card h-100">
-      <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp" class="card-img-top" alt="Skyscrapers"/>
-      <div class="card-body">
-        <h5 class="card-title">Hod Portal</h5>
-       
-        <Link to="/Hodregister" style={{backgroundColor:"#007a99"}} class="btn btn-primary"><b>Login as HOD</b></Link>
- 
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card h-100">
-      <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/043.webp" class="card-img-top" alt="Los Angeles Skyscrapers"/>
-      <div class="card-body">
-        <h5 class="card-title">Warden Portal</h5>
-        <Link to="/wardenregister" style={{backgroundColor:"#007a99"}} class="btn btn-primary"><b>Login as Warden</b></Link>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card h-100">
-      <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp" class="card-img-top" alt="Palm Springs Road"/>
-      <div class="card-body">
-        <h5 class="card-title">Student Portal</h5>
-        <Link to="Login" style={{backgroundColor:"#007a99"}} class="btn btn-primary"><b>Login as Student</b></Link>
-      </div>
-      
-    </div>
-  </div>
-</div> */}
-        </div>
+
+{/* <Router>
+
+</Router> */}
+        </>
     )
 }
 
