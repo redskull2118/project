c3ccae1ea86d5cddeaef0bcc3dc291e5cb5ff4e5
redskull2118 <118445883+redskull2118@users.noreply.github.com>
2023-05-25 09:55:41 +0530

    Add files via upload

--- Home.js
+++ Home.js
@@ -1,47 +1,281 @@
 import {Link} from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import StudentApplyLeaveNavbar from './StudentApplyLeaveNavbar'
 // import WorkingDayLeave from './WorkingDayLeave'
+import '../App.css';
+import StudentApplyLeave from './StudentApplyLeave'
+import AWDL from './AWDL';
 
 function Home(){
+  // const [users, setUsers] = useState([]);
+  
+  // async function login(){
+
+  //   const response = await fetch('http://localhost:8000/abc', {
+  //     method:'GET',
+  //     headers:{
+  //       'Content-Type':'application/json'
+  //     }
+     
+  //   });
+  //   const actualdata = await response.json();
+  //   setUsers(actualdata.data);
+  //   console.log(actualdata.data);
+  //   // return data;
+  // } 
+
+  // useEffect(()=>{
+  //   login();
+  // },[])
+
+  // const color=(a)=>{
+  //   if(a=='pending')
+  //   {
+  //     return(
+  //       <>
+  //       <p style={{color:"yellow"}}> Pending</p>
+  //       </>
+  //     )
+  //   }
+  //   else if(a=='approved')
+  //   {
+  //     return(
+  //       <>
+  //       <p style={{color:"green"}}>Approved</p>
+  //       </>
+  //     )
+  //   }
+  //   else{
+  //     return(
+  //       <>
+  //       <p style={{color:"red"}}>Rejected</p>
+  //       </>
+  //     )
+
+  //   }
+  // }
+
     return(
         <>
         <StudentApplyLeaveNavbar/>
-        <div style={{display:'flex', marginLeft:30+'px',marginTop:40+'px', border:1+'px solid black',marginRight:30+'px' }}>
-        <div class="card"  style={{width: 18+'rem',marginLeft:110+'px'}}>
-  <div class="card-body">
-    <h1 class="card-title">Leaves</h1>
-    <h6 class="card-subtitle mb-2 text-body-secondary">  </h6>
-    <p class="card-text"><h4>Remaining Leave: 20/20</h4></p>
+      
+      <div >
+      {/* <div class="row " style={{margin:70+'px',marginTop:30+'px'}}>
+  <div class="col sha hov " >
+    <div class="card h-100 border comcol  " style={{border:'none'}}>
+      
+     <div class="card-body comcol ">
+        <center><h2 class="card-title" style={{paddingTop:20+'px', paddingBottom:20+'px', color:" #0E2431"}}><b>Remaining Leaves</b></h2></center>
+        
+      </div>
+     
+    </div>
+  </div>
+
+  <div class="col sha hov " >
+    <div class="card h-100 border comcol  " style={{border:'none'}}>
+      
+     <div class="card-body comcol " style={{paddingTop:37+'px'}}>
+        <center><h2 class="card-title " ><Link to="/studentapplyleave" className='leave'>Remaining Leaves</Link></h2></center>
+        
+      </div>
+     
+    </div>
+  </div>
+
+
+  <div class="col sha hov">
+    <div class="card h-100 comcol border" style={{border:'none'}}>
+      <div class="card-body comcol">
+      <center><h2 class="card-title" style={{paddingTop:20+'px', paddingBottom:20+'px'}}>
+
+      <button type="text"  data-mdb-toggle="modal" data-mdb-target="#exampleModal" style={{background:'none',boxShadow:'none',paddingTop:8+'px', border:'none', color:" #0E2431"}}>
+ Apply Working Day leave 
+</button>
+
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Apply Leave</h5>
+        <button type="button" class="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body"><AWDL/></div>
+      
+    </div>
+  </div>
+</div>
+        
+        
+        </h2></center>
+      </div>
+      
+    </div>
+  </div> 
+   <div class="col sha hov">
+    <div class="card h-100 comcol border" style={{border:'none'}}>
+      <div class="card-body comcol ">
+      <center><h2 class="card-title" style={{paddingTop:20+'px', paddingBottom:20+'px'}}>
+      <button type="text"  data-mdb-toggle="modal" data-mdb-target="#exampleModal" style={{background:'none',boxShadow:'none',paddingTop:8+'px', border:'none', color:" #0E2431"}}>
+ Apply holiday leave 
+</button>
+
+
+<div class="modal fade " id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header ap-col">
+        <h5 class="modal-title ap-col" id="exampleModalLabel">Apply Leave</h5>
+        <button type="button" class="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body"><StudentApplyLeave/></div>
+      
+    </div>
+  </div>
+</div>
+        
+        
+        </h2></center>
+      </div>
+      
+    </div>
+  </div>
+</div> */}
+
+
+<div class="row row-cols-1 row-cols-md-3 g-4" style={{margin:70+'px',marginTop:30+'px'}}>
+  <div class="col sha hov">
+    <div class="card h-100 comcol border" style={{border:'none'}}>
+      
+     <div class="card-body comcol" >
+     <center><h2 class="card-title" style={{paddingTop:20+'px', paddingBottom:20+'px', color:" #0E2431"}}><b>Remaining Leaves</b></h2></center>
+      </div>
+     
+    </div>
+  </div>
+  
+  <div class="col sha hov">
+    <div class="card h-100 comcol border" style={{border:'none'}}>
+    <Link to='/studentworkingdayleave' style={{textDecoration:"none"}}>
+    <div class="card-body comcol">
+      <center><h2 class="card-title" style={{paddingTop:20+'px', paddingBottom:20+'px', color:" #0E2431",textDecoration:"none"}}><b>View H L</b></h2></center>
+      </div>
+    </Link> 
+    </div>
+  </div>
+
+  <div class="col sha hov">
+    <div class="card h-100 comcol border" style={{border:'none'}}>
+    <Link to='/studentworkingdayleave' style={{textDecoration:"none"}}>
+    <div class="card-body comcol">
+      <center><h2 class="card-title" style={{paddingTop:20+'px', paddingBottom:20+'px', color:" #0E2431",textDecoration:"none"}}><b>View W D L</b></h2></center>
+      </div>
+    </Link>
+      
+    </div>
+  </div>
+</div>
+
+
+<div class="row row-cols-1 row-cols-md-3 g-4" style={{margin:70+'px'}}>
+  <div class="col sha hov">
+    <div class="card h-100 comcol border" style={{border:'none'}}>
+      
+    <Link to='/studentapplywdleave' style={{textDecoration:"none"}}>
+    <div class="card-body comcol">
+      <center><h2 class="card-title" style={{paddingTop:20+'px', paddingBottom:20+'px', color:" #0E2431",textDecoration:"none"}}><b>Apply W D L</b></h2></center>
+      </div>
+    </Link>
+     
+    </div>
+  </div>
+  <div class="col sha hov">
+    <div class="card h-100 comcol border" style={{border:'none'}}>
+    <Link to='/studentapplyleave' style={{textDecoration:"none"}}>
+    <div class="card-body comcol">
+      <center><h2 class="card-title" style={{paddingTop:20+'px', paddingBottom:20+'px', color:" #0E2431",textDecoration:"none"}}><b>Apply H L</b></h2></center>
+      </div>
+    </Link>
+      
+    </div>
+  </div>
+  <div class="col sha hov">
+    <div class="card h-100 comcol border" style={{border:'none'}}>
+      <div class="card-body comcol" style={{pointerEvents:"none"}}>
+      <center><h2 class="card-title" style={{paddingTop:20+'px', paddingBottom:20+'px', color:" #0E2431"}}><b>Calender</b></h2></center>
+      </div>
+      
+    </div>
+  </div>
+</div>
+
+
+<div class="row row-cols-1 row-cols-md-3 g-4" style={{margin:70+'px'}}>
+  <div class="col sha hov">
+    <div class="card h-100 comcol border" style={{border:'none'}}>
+      
+     <div class="card-body comcol" style={{pointerEvents:"none"}}>
+     <center><h2 class="card-title" style={{paddingTop:20+'px', paddingBottom:20+'px', color:" #0E2431"}}><b>Sports</b></h2></center>
+      </div>
+     
+    </div>
+  </div>
+  <div class="col sha hov ">
+    <div class="card h-100 comcol border" style={{border:'none'}}>
+      <div class="card-body comcol" style={{pointerEvents:"none"}}>
+      <center><h2 class="card-title" style={{paddingTop:20+'px', paddingBottom:20+'px', color:" #0E2431"}}><b>Assignment</b></h2></center> </div>
+      
+    </div>
+  </div>
+  <div class="col sha hov ">
+    <div class="card h-100 comcol border" style={{border:'none'}}>
+      <div class="card-body comcol" style={{pointerEvents:"none"}}>
+      <center><h2 class="card-title" style={{paddingTop:20+'px', paddingBottom:20+'px', color:" #0E2431"}}><b>Change Password</b></h2></center>
+      </div>
+      
+    </div>
+  </div>
+</div>
+</div>
+{/* // <center><h2 style={{marginTop:15+'px',marginBottom:20+'px'}}>Working DAY LEAVE</h2></center>
+
+//       <table class="table " style={{backgroundColor:"rgb(245, 245, 245)",fontFamily:"'Poppins',sans-serif"}}>
+//   <thead style={{backgroundColor:"rgb(222, 238, 252)",color:"#0E2431"}}>
+//     <tr>
+//       <th scope="col">Student Name</th>
+//       <th scope="col">Start Date</th>
+//       <th scope="col">End Date</th>
+//       <th scope="col">Type of Leave</th>
+//       <th scope="col"> WARDEN's Status</th>
+//       <th scope="col"> HOD's Status</th>
+      
+//     </tr>
+//   </thead>
+//   <tbody style={{color:"#6883BC" ,fontFamily:"'Poppins',sans-serif"}}> */}
+   {/* {users.map(a=>{ */}
+{/* //           return(
+//             <tr>
+//       <td>{a.studentname}</td>
+//       <td>{a.startdate}</td>
+//       <td>{a.enddate}</td>
+//       <td>{a.purpose}</td>
+//       <td>{color(a.status)}</td>
+//       <td>{color(a.status)}</td>
+//     </tr>
+
+
+//         // <li>{a.studentname}</li>
+         
+//         )  
+//       })}
     
-  </div>
-</div>
+    
+//   </tbody>
 
-<div class="card"  style={{width: 18+'rem',marginLeft:110+'px'}}>
-  <div class="card-body">
-    <h5 class="card-title">Working Day Leave </h5>
-    <h6 class="card-subtitle mb-2 text-body-secondary"></h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    {/* <Link to="/studentlogin">W D L</Link> */}
-    <Link to="/studentworkingdayleave">W D L</Link>
-    {/* <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a> */}
-  </div>
-</div>
+// </table> */}
 
-<div class="card"  style={{width: 18+'rem',marginLeft:110+'px'}}>
-  <div class="card-body">
-    <h5 class="card-title">Holiday Leave</h5>
-    <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-</div>
-</div>
 
-{/* <Router>
-
-</Router> */}
         </>
     )
 }
