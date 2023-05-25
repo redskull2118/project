b63aef44eaf6b7d832ecb807973238e06a61a315
redskull2118 <118445883+redskull2118@users.noreply.github.com>
2023-05-25 09:59:54 +0530

    Add files via upload

--- StudentApplyLeaveNavbar.js
+++ StudentApplyLeaveNavbar.js
@@ -1,20 +1,37 @@
 import StudentApplyLeave from './StudentApplyLeave'
+import '../App.css';
+import { Link } from 'react-router-dom';
+import Home from './Home';
 
 function StudentApplyLeaveNavbar(){
     return(
         <>
-           <nav class="navbar navbar-expand-lg navbar-light bg-light">
+           <nav class="navbar navbar-expand-lg navbar-light navcol ">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Student Portal</a>
+    <a class="navbar-brand hhh" href="#">Student Portal</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+
+        <li class="nav-item adj hhh">
+          <Link to='/' style={{textDecoration:"none", color:"#0E2431"}}>Home</Link>
         </li>
-        <li class="nav-item">
+      
+        <li class="nav-item adj hhh"  >
+        <Link  style={{textDecoration:"none", color:"#0E2431"}} >Anything</Link>
+        </li>
+
+        <li class="nav-item adj hhh">
+        <Link  style={{textDecoration:"none", color:"#0E2431"}}>Anything</Link>
+        </li>
+
+        <li class="nav-item adj hhh">
+        <Link  style={{textDecoration:"none", color:"#0E2431"}}>Logout</Link>
+        </li>
+
+        {/* <li class="nav-item">
        
 <button type="text"  data-mdb-toggle="modal" data-mdb-target="#exampleModal" style={{background:'none',boxShadow:'none',paddingTop:8+'px', border:'none'}}>
  Appy holiday leave 
@@ -36,9 +53,9 @@ function StudentApplyLeaveNavbar(){
     </div>
   </div>
 </div>
-        </li>
+        </li> */}
 
-        <li class="nav-item">
+        {/* <li class="nav-item">
        
        <button type="text"  data-mdb-toggle="modal" data-mdb-target="#exampleModal" style={{background:'none',boxShadow:'none',paddingTop:8+'px', border:'none'}}>
          Apply WD Leave
@@ -46,7 +63,7 @@ function StudentApplyLeaveNavbar(){
        {/* <p data-mdb-toggle="modal" data-mdb-target="#exampleModal">apply</p> */}
        
        
-       <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+       {/* <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
          <div class="modal-dialog">
            <div class="modal-content">
              <div class="modal-header">
@@ -61,12 +78,13 @@ function StudentApplyLeaveNavbar(){
            </div>
          </div>
        </div>
-               </li>
+               </li> */}
 
 
-        <li class="nav-item">
-          <a class="nav-link" href="#">Logout</a>
-        </li>
+        {/* <li class="nav-item">
+          <a class="nav-link " href="#" style={{fontFamily:"'Poppins',sans-serif",fontSize: 20+"px",color:"#0E2431"}}>Logout</a>
+        </li> */}
+        
       
       </ul>
     </div>
